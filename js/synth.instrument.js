29fcf16d1c593e7f8e8b42719c0ca3bf1018ce2a
Warren Galyen <wgalyen@gmail.com>
2020-12-27 15:34:44 -0800

    Added separate voices for each set of oscs, filters and gain nodes Makes it properly polyphonic. Played notes cycle through which voice they use.

--- js/synth.instrument.js
+++ js/synth.instrument.js
@@ -3,9 +3,13 @@ let synth = function (config) {
     this.instrumentName = 'Synth101';
     this.instrumentID = config.instrumentID || Date.now();
     this.context = config.context;
-    this.gainNode = this.context.createGain();
+    this.masterGainNode = this.context.createGain();
     this.notes = {};
+    this.noteVoiceLog = {};
+
     this.polyphony = 8;
+    this.oscsPerVoice = 2;
+    this.lastVoice = 0;
 
     this.oscNodes = [];
     this.ampNodes = [];
@@ -28,10 +32,10 @@ synth.prototype = {
     init: function () {
 
         //Set master volume of this instrument connect gain
-        this.gainNode.gain.value = 0.3;
-        this.gainNode.connect(this.context.destination);
+        this.masterGainNode.gain.value = 0.3;
 
         this.createNodes();
+        this.connectNodes();
 
         //Set initial values of all controls
         this.initControlValues();
@@ -40,20 +44,62 @@ synth.prototype = {
 
     //-------
 
-    createNodes: function() {
+    createNodes: function () {
 
-        // Loop through this voices count (polyphony) and create oscillator, filter and gain nodes
+        //Loop through the voices count (polyphony) and create oscillator, filter and gain nodes
         for (let i = 0; i < this.polyphony; i++) {
-            // 2 Oscillators for each voice
-            let voice = [this.context.createOscillator(), this.context.createOscillator()];
 
+            //2 Osc's for each voice
+            let voice = [
+                this.context.createOscillator(),
+                this.context.createOscillator()
+            ];
+
+            //Start oscs playing
+            for (let key in voice) {
+                voice[key].start();
+            }
+
+            //Create amp (gain) node and set volume to 0
             let amp = this.context.createGain();
+            amp.gain.value = 0;
+
+            //Create filter node
             let filter = this.context.createBiquadFilter();
+            filter.frequency.value = 3000;
+            filter.type = 'lowpass';
 
             this.oscNodes.push(voice);
             this.ampNodes.push(amp);
             this.filterNodes.push(filter);
+
         }
+
+    },
+
+    //-------
+
+    connectNodes: function () {
+
+        //Loop through each voice and connect the nodes together
+        for (let i = 0; i < this.polyphony; i++) {
+
+            //Connect the 2 osc nodes for this voice to the voice amp (gain) node
+            this.oscNodes[i][0].connect(this.ampNodes[i]);
+            this.oscNodes[i][1].connect(this.ampNodes[i]);
+
+            //Connect the amp node for this voice to the filter node
+            this.ampNodes[i].connect(this.filterNodes[i]);
+
+            //Connect the filter node to the master gain
+            this.filterNodes[i].connect(this.masterGainNode);
+
+            //TODO - Connect master gain node to a mixer channel rather than directly to destination
+            //Connect master gain node to destination (speakers)
+            this.masterGainNode.connect(this.context.destination);
+
+        }
+
     },
 
     //-------
@@ -80,51 +126,80 @@ synth.prototype = {
                 //Set Res
                 break;
         }
-    },
-
-    //-------
-
-    connectNodes: function () {
+        ;
 
     },
 
     //-------
 
     noteOn: function (noteNumber, velocity) {
+
+        //Select a voice to use
+        let currentVoice = this.lastVoice + 1;
+        if (currentVoice >= this.polyphony) {
+            currentVoice = 0;
+        }
+        this.lastVoice = currentVoice;
+
+        //Keep a log which voice this note is using (This can then be used on noteOff)
+        this.noteVoiceLog[noteNumber] = currentVoice;
+
+        console.log(noteNumber + ' using voice: ' + currentVoice);
+
+        //Get frequency of midi note and note start time
         let frequency = app.midiNoteToFrequency(noteNumber);
         let startTime = this.context.currentTime;
 
-        //Array of 2 oscillators
-        this.notes[noteNumber] = [
-            this.context.createOscillator(),
-            this.context.createOscillator(),
-        ];
+        let ampNode = this.ampNodes[currentVoice];
+        let filterNode = this.filterNodes[currentVoice];
 
-        //Loop through the 2 oscillators
-        for (let key in this.notes[noteNumber]) {
 
-            //Set values of oscillator
-            this.notes[noteNumber][key].frequency.value = frequency;
-            this.notes[noteNumber][key].type = 'sawtooth';
-
-            //Tuning for second oscillator
-            if (key > 0) {
-                this.notes[noteNumber][key].detune.value = 6;
+        //Set frequecy of the 2 oscillators for this voice
+        let oscNode;
+        for (let i = 0; i < this.oscsPerVoice; i++) {
+            oscNode = this.oscNodes[currentVoice][i];
+            oscNode.frequency.setValueAtTime(frequency, startTime + 0.01);
+            oscNode.type = 'sawtooth';
+            //Tuning for second osc
+            if (i === 1) {
+                oscNode.detune.value = 6;
             }
-
-            //Connect the oscillator to the gain node and start playing
-            this.notes[noteNumber][key].connect(this.gainNode);
-            this.notes[noteNumber][key].start(startTime);
-
         }
+
+        //Amp envelope
+        //ampNode.gain.setValueAtTime(0, startTime);
+        ampNode.gain.cancelScheduledValues(startTime);
+        ampNode.gain.linearRampToValueAtTime(0, startTime + 0.01);
+        ampNode.gain.linearRampToValueAtTime(1, startTime + 0.05);
+
+        //Filter envelope
+        filterNode.frequency.cancelScheduledValues(startTime);
+        filterNode.frequency.linearRampToValueAtTime(0, startTime + 0.01);
+        filterNode.frequency.linearRampToValueAtTime(3000, startTime + 0.02);
 
     },
 
     //-------
 
     noteOff: function (noteNumber) {
-        this.notes[noteNumber][0].stop(this.context.currentTime);
-        this.notes[noteNumber][1].stop(this.context.currentTime);
+        //Find which voice was assigned to this note in the log
+        if (typeof this.noteVoiceLog[noteNumber] === undefined) {
+            console.log('Could not find which voice is used for note: ' + noteNumber);
+            return;
+        }
+
+        let voice = this.noteVoiceLog[noteNumber];
+        console.log('Stopping voice: ' + voice);
+
+        let currentTime = this.context.currentTime;
+
+        //this.ampNodes[voice].gain.cancelScheduledValues(currentTime);
+        this.ampNodes[voice].gain.exponentialRampToValueAtTime(0.000001, currentTime + 6);
+
+        this.filterNodes[voice].frequency.exponentialRampToValueAtTime(0.000001, currentTime + 4);
+
+        //Remove from the log
+        delete this.noteVoiceLog[noteNumber];
     }
 
 };
